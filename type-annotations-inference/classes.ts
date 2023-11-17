f3b03343f0181e99df7903890caf19d749a09211
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-11-17 13:54:47 -0300

    added field to parend class Vehicle + added field inheritance on child class vehicle

--- type-annotations-inference/classes.ts
+++ type-annotations-inference/classes.ts
@@ -1,20 +1,38 @@
 class Vehicle {
+  constructor(public color: string, public horsePower: number) {}
+
   protected honk(): void {
     console.log('Biiiiiiiii');
   }
+
+  protected logCarColor(): void {
+    console.log(this.color);
+  }
+
+  protected logCarHorsePower(): void {
+    console.log(this.horsePower);
+  }
 }
+
+//----------------------------------------------------------------------------
 
 // Extends - Vehicle is the "super class" or "parent class"
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string, horsePower: number) {
+    super(color, horsePower);
+  }
+
   private drive(): void {
     console.log('vruuum from car!');
   }
 
   public startDrivingProcess(): void {
+    this.logCarColor();
+    this.logCarHorsePower();
     this.drive();
     this.honk();
   }
 }
 
-const car = new Car();
+const car = new Car(4, 'orange', 15000);
 car.startDrivingProcess();
